19840564cbe2b11ddeeab4981edee7df092ec960
Dmitrii Trebin <d.trebin@rambler.ru>
2022-06-01 20:35:54 +0300

    Add Tests

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackerTest {
+
+    CashbackHacker cashbackHacker = new CashbackHacker();
+
+    @Test
+    public void cashbackl(){
+
+        int expected = 100;
+        int actual = cashbackHacker.remain(900);
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void cashbackLimit(){
+        int expected = 1000;
+        int actual = cashbackHacker.remain(0);
+
+        assertEquals(actual,expected);
+
+    }
+
+}
